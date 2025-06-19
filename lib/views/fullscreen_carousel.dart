03241d42e3161acc45a915ac48b707fbcb73c641
Philip Frerk <philip.frerk@icloud.com>
2025-06-19 21:32:01 +0300

    show current page in fullscreen mode

--- lib/views/fullscreen_carousel.dart
+++ lib/views/fullscreen_carousel.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:michelle_frerk/views/media_viewer.dart';
 import 'package:michelle_frerk/models/media_item.dart';
 
-class FullScreenCarousel extends StatelessWidget {
+class FullScreenCarousel extends StatefulWidget {
   final List<MediaItem> mediaItems;
   final int initialPage;
 
@@ -13,24 +13,70 @@ class FullScreenCarousel extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: initialPage);
+  State<FullScreenCarousel> createState() => _FullScreenCarouselState();
+}
 
+class _FullScreenCarouselState extends State<FullScreenCarousel> {
+  late PageController _controller;
+  late int _currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentPage = widget.initialPage;
+    _controller = PageController(initialPage: widget.initialPage);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: PageView.builder(
-        controller: controller,
-        itemCount: mediaItems.length,
-        itemBuilder: (context, index) {
-          final media = mediaItems[index];
-          return Center(
-            child: MediaViewer(mediaItem: media),
-          );
-        },
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _controller,
+            itemCount: widget.mediaItems.length,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              final media = widget.mediaItems[index];
+              return Center(
+                child: MediaViewer(mediaItem: media),
+              );
+            },
+          ),
+          Positioned(
+            bottom: 24,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  '${_currentPage + 1} / ${widget.mediaItems.length}',
+                  style: const TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
